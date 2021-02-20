cf2ba30c8dc2d9442bf6111b7ebe73abff4939ab
woodelin-florveus <woodelinflorveus@gmail.com>
2021-02-20 10:34:27 -0500

    done

--- src/Comment.js
+++ src/Comment.js
@@ -1,7 +1,17 @@
 import React from "react";
 
-function Comment() {
-  return <div>Naturally, I agree with this article.</div>;
+// function Comment() {
+//   return <div>Naturally, I agree with this article.</div>;
+// }
+
+
+// const Comment = () => <div> Naturally, I agree with this article</div>
+
+// class example 
+class Comment extends React.Component {
+    render(){
+      return <div>Naturally, I argree with this article.</div>
+    }
 }
 
 export default Comment;
